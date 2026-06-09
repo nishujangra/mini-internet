323c4e935d39fb9ba90d57a2a1e947373372d99b
nishujangra <ndjangra1027@gmail.com>
2026-06-09 16:44:46 +0530

    feat: load and print router config from yaml on startup

--- cmd/mini-internet/main.go
+++ cmd/mini-internet/main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/goccy/go-yaml"
+	"github.com/nishujangra/mini-internet/internal/models"
+)
 
 func main() {
-	fmt.Println("My mini internet")
+	confFile, err := ioutil.ReadFile("configs/router.yaml")
+	if err != nil {
+		log.Printf("confFile err   #%v ", err)
+	}
+
+	var config models.Config
+
+	err = yaml.Unmarshal(confFile, &config)
+	if err != nil {
+		log.Printf("unmarshal err   #%v ", err)
+	}
+
+	fmt.Printf("Router ID: %s\n", config.Router.ID)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/nishujangra/mini-internet
 
 go 1.26.3
+
+require github.com/goccy/go-yaml v1.19.2
