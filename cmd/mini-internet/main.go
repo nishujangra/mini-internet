8402ffac27dd0fa4097e3cd61b36a858012da6ad
nishujangra <ndjangra1027@gmail.com>
2026-06-09 23:20:45 +0530

    feat: add TCP listener and accept loop for incoming neighbor connections

--- cmd/mini-internet/main.go
+++ cmd/mini-internet/main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
+	"net"
+	"os"
 
 	"github.com/goccy/go-yaml"
 	"github.com/nishujangra/mini-internet/internal/models"
 )
 
 func main() {
-	confFile, err := ioutil.ReadFile("configs/router.yaml")
+	confFile, err := os.ReadFile("configs/router.yaml")
 	if err != nil {
 		log.Printf("confFile err   #%v ", err)
 	}
@@ -23,4 +24,29 @@ func main() {
 	}
 
 	fmt.Printf("Router ID: %s\n", config.Router.ID)
+	for i, neigh := range config.Neighbors {
+		fmt.Printf("Address of Neighbor %d is %v\n", i, neigh)
+	}
+
+	// Start the listner (default listen at tcp)
+	ln, err := net.Listen("tcp", fmt.Sprintf("%s:%s", config.Router.ListenIP, config.Router.Port))
+	if err != nil {
+		fmt.Printf("Error in starting listner")
+	}
+
+	// Accept connections
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Printf("accept error: %v", err)
+			continue
+		}
+
+		log.Printf(
+			"neighbor connected from %s",
+			conn.RemoteAddr(),
+		)
+
+		conn.Close()
+	}
 }
