f6468f6cb91e0c46d28708680e37acd84465adca
nishujangra <ndjangra1027@gmail.com>
2026-06-25 00:23:55 +0530

    feat: add RIBManager and HTTP /routes endpoint for show routes

--- cmd/mini-internet/main.go
+++ cmd/mini-internet/main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net"
+	"net/http"
+	"net/netip"
 	"os"
 
 	"github.com/goccy/go-yaml"
 	"github.com/nishujangra/mini-internet/internal/models"
 	"github.com/nishujangra/mini-internet/pkg/neighbor"
+	"github.com/nishujangra/mini-internet/pkg/rib"
 )
 
 func main() {
@@ -36,6 +40,23 @@ func main() {
 	}
 
 	nmg := neighbor.NewNeighborManager()
+	ribmg := rib.NewRIBManager()
+
+	// TEST
+	ribmg.Insert(models.Route{
+		Prefix:        netip.MustParsePrefix("10.0.0.0/24"),
+		NextHop:       netip.MustParseAddr("192.168.1.1"),
+		Protocol:      models.OSPF,
+		AdminDistance: 110,
+		Metric:        10,
+	})
+
+	go func() {
+		http.HandleFunc("/routes", func(w http.ResponseWriter, r *http.Request) {
+			json.NewEncoder(w).Encode(ribmg.List())
+		})
+		http.ListenAndServe(":8080", nil)
+	}()
 
 	// Accept connections
 	for {
